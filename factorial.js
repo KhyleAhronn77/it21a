3fc2e989f9837c84ddfdf0c05c98faffbb277ed3
KhyleAhronn77 <kindomkhyleahron@gmail.com>
2025-09-08 08:08:21 +0800

    Added logic and computation for factorial.js

--- factorial.js
+++ factorial.js
@@ -17,9 +17,24 @@ rl.question("Enter your choice (1-3)", choice =>{
         console.log("hello There!");
     } else if (choice==="2"){
         console.log("factorial");
+        rl.question("Enter a number for factorial: ", (numStr)=>{
+            let num = parseInt(numStr);
+
+            if (isNaN() ) || num <0){
+                console.log("Invalid number. Please try again");
+            } else{
+                let fact =1;
+
+                for (let i=2; i<=num; i++){
+                    fact *=i;
+                }
+
+                console.log(The factorial of "+ num + " is " + fact);
+        }
     } else if (choice=="3"){
         console.log ("exit!");
     } else{
+
   
     console.log("Invalid Choice.Please Try Again");
 
