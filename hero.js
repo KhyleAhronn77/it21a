4adc279c043fa1de654076b4eeb139c1377646c7
KhyleAhronn77 <kindomkhyleahron@gmail.com>
2025-09-15 08:43:39 +0800

    Added mage subclass and updated object instance

--- hero.js
+++ hero.js
@@ -17,7 +17,20 @@ class Warrior extends Hero{
         console.log(`${this.name} uses power Strike`);
 
     }
-
 }
+class Mage extends Hero{
+    constructor(name,health,attack,mana){
+        super(name,health,attack);
+        this.mana = mana;
+    }
+    useAbility(){
+        console.log(`${this.name}casts Fireball!`);
+
+    }
+}
+
 const warrior = new Warrior("Thorin",100,10);
 warrior.useAbility()
+
+const mage = new Mage("Gandalf",80,8,50);
+mage.useAbility();
